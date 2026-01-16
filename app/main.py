d4e6f1ff516513a9971ec78d46caf211383248ec
Jyanhwei <wangjh9712@gmail.com>
2026-01-16 15:07:56 +0800

    新增：前端媒体库，元数据加载

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from app.routers import meta
 from app.routers import resources
 from app.routers import p115
 
-app = FastAPI(title="Fullbr115 API")
+app = FastAPI(title="Fullbr115")
 
 app.include_router(meta.router)
 app.include_router(resources.router)
 app.include_router(p115.router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def read_index():
+    return FileResponse('static/index.html')
 
 if __name__ == "__main__":
     import uvicorn
